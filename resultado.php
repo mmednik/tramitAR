ce31dd08693f3b0db9e44fef096359cb33439214
Martín Alejandro Mednik <mmednik@gmail.com>
2013-08-04 09:28:25 -0300

    Update resultado.php

--- resultado.php
+++ resultado.php
@@ -1,9 +1,8 @@
 <?php 
 	include('lib/class.MySQL.php');
-	$oMySQL = new MySQL('tramites','root','mysql');
+	$oMySQL = new MySQL('tramites','***','***');
 	$resultSearch = $oMySQL->ExecuteSQL('SELECT idtramites FROM tramites WHERE titulo ="' . $_POST['datum'] . '"');
 	$html = intval($resultSearch['idtramites']);
-	//$html = 'SELECT idtramites FROM tramites WHERE titulo ="' . $_POST['datum'] . '"';
 	echo $html;
 ?>
     
